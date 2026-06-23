e4abc0f08735cfbaac830bf20b86c15c312a95d8
Alen Boryan <alenboryan2007@gmail.com>
2026-06-23 16:17:19 +0400

    Move SECRET_KEY to .env file for security

--- config/settings.py
+++ config/settings.py
@@ -11,19 +11,16 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fbsp48%gdc1cqha$vx_irtsp@-k5ntoq6f)%jk!n@u7mwl@@+9'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -123,7 +120,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 from datetime import timedelta
-import os 
 
 STATIC_URL = 'static/'
 SIMPLE_JWT = {
